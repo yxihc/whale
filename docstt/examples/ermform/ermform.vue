c270eefb1ecec739d5f0e1b6063f2727325f3c18
月夕花晨 <825210484@qq.com>
2024-06-03 13:35:32 +0800

    删除多余文件

--- docstt/examples/ermform/ermform.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <wl-erm-form title="测试" />
-  </div>
-</template>
-
-<script setup type="ts">
-import WlErmForm from "@whale/components/erm-form";
-</script>
--- docstt/team.md
+++
@@ -1,9 +0,0 @@
----
-layout: page
-title: 认识我们的团队
-description:
----
-
-
-<WhaleTeam></WhaleTeam>
-
